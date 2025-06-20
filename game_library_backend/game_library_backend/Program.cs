cb4b43b6edc110f07002824bda6a5dc1a55eeb57
João Marcello <marcello.jao1611@gmail.com>
2025-06-20 02:29:29 -0300

    Add Swagger and routing to endpoints

--- game_library_backend/game_library_backend/Program.cs
+++ game_library_backend/game_library_backend/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -12,9 +15,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+app.UseRouting();
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Game Library API V1");
+    });
 }
 
 app.Run();
