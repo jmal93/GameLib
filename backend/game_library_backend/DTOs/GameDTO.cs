5ae1532e82f1affff4675a3e8e8e31dcccb445fe
João Marcello <marcello.jao1611@gmail.com>
2025-06-25 01:12:15 -0300

    Add DTOs

---
+++ backend/game_library_backend/DTOs/GameDTO.cs
@@ -0,0 +1,12 @@
+namespace game_library_backend.DTOs;
+
+public class GameDTO
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public decimal? Price { get; set; }
+    public required string Developer { get; set; }
+    public string? Image { get; set; }
+    public required List<GenreDTO> Genres { get; set; }
+}
---
+++ backend/game_library_backend/DTOs/GenreDTO.cs
@@ -0,0 +1,8 @@
+namespace game_library_backend.DTOs;
+
+public class GenreDTO
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+
+}
