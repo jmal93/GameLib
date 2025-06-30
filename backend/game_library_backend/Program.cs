4495048bb6cfae90ec82ced5684b296905983ca6
João Marcello <marcello.jao1611@gmail.com>
2025-06-30 01:46:47 -0300

    Apply pending migrations

--- backend/game_library_backend/Program.cs
+++ backend/game_library_backend/Program.cs
@@ -122,5 +122,12 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+
 app.Run();
 
