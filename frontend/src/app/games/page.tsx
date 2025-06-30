4293d445236852de34f162e2b34194bda2f2784f
João Marcello <marcello.jao1611@gmail.com>
2025-06-29 23:04:00 -0300

    Show if there are no games

--- frontend/src/app/games/page.tsx
+++ frontend/src/app/games/page.tsx
@@ -3,6 +3,11 @@ import { GameService } from "@/services/api";
 
 export default async function Library() {
   const games = await GameService.getAllGames();
+
+  if (games.length == 0) {
+    return <h2 className="text-center py-4 text-3xl">Empty game list</h2>;
+  }
+
   return (
     <div>
       <h1 className="text-center text-4xl">Games</h1>
@@ -10,3 +15,5 @@ export default async function Library() {
     </div>
   );
 }
+
+export const dynamic = "force-dynamic";
